55fba8d8db7f7532e5de90a923243d20f4e26b73
Sam <sgpinkus@gmail.com>
2026-03-05 19:44:39 +1100

    Add Sensor attrs.

--- custom_components/myaemodata/const.py
+++ custom_components/myaemodata/const.py
@@ -1,17 +1,49 @@
 """Constants for myaemodata."""
 
+from typing import Any, Literal
 from logging import Logger, getLogger
+from homeassistant.const import (
+    CURRENCY_DOLLAR,
+    UnitOfEnergy,
+)
+from homeassistant.components.sensor import SensorDeviceClass
+
+MetricNameType = Literal["RRP", "TOTALDEMAND", "NETINTERCHANGE", "SCHEDULEDGENERATION", "SEMISCHEDULEDGENERATION", "APCFLAG"]
 
 LOGGER: Logger = getLogger(__package__)
 DOMAIN = "myaemodata"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
-METRICS = [
-    "RRP",
-    "TOTALDEMAND",
-    "NETINTERCHANGE",
-    "SCHEDULEDGENERATION",
-    "SEMISCHEDULEDGENERATION",
-    "APCFLAG",
-]
+METRICS: dict[MetricNameType, dict[str, Any]] = {
+    "RRP": {
+        "unit": f"{CURRENCY_DOLLAR}/{UnitOfEnergy.MEGA_WATT_HOUR}",
+        "device_class": SensorDeviceClass.MONETARY,
+        "display_precision": 2,
+    },
+    "TOTALDEMAND": {
+        "unit": UnitOfEnergy.MEGA_WATT_HOUR,
+        "device_class": SensorDeviceClass.POWER,
+        "display_precision": 3,
+    },
+    "NETINTERCHANGE": {
+        "unit": UnitOfEnergy.MEGA_WATT_HOUR,
+        "device_class": SensorDeviceClass.POWER,
+        "display_precision": 3,
+    },
+    "SCHEDULEDGENERATION": {
+        "unit": UnitOfEnergy.MEGA_WATT_HOUR,
+        "device_class": SensorDeviceClass.POWER,
+        "display_precision": 3,
+    },
+    "SEMISCHEDULEDGENERATION": {
+        "unit": UnitOfEnergy.MEGA_WATT_HOUR,
+        "device_class": SensorDeviceClass.POWER,
+        "display_precision": 3,
+    },
+    "APCFLAG": {
+        "unit": None,
+        "device_class": None,
+        "display_precision": 0,
+    },
+}
 REGIONS = ["NSW1", "QLD1", "VIC1", "SA1", "TAS1"]
 PERIODS = ["ACTUAL", "FORECAST"]
--- custom_components/myaemodata/sensor.py
+++ custom_components/myaemodata/sensor.py
@@ -3,7 +3,10 @@ from homeassistant.core import HomeAssistant, callback
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from .const import METRICS, REGIONS, PERIODS
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.components.sensor import SensorStateClass
+
+from .const import DOMAIN, METRICS, REGIONS, PERIODS
 
 
 async def async_setup_entry(
@@ -12,7 +15,7 @@ async def async_setup_entry(
     coordinator = entry.runtime_data
     entities = []
     for period in PERIODS:
-        for metric in METRICS:
+        for metric in METRICS.keys():
             for region in REGIONS:
                 entities.append(RegionSensor(coordinator, period, metric, region))
     async_add_entities(entities)
@@ -26,28 +29,33 @@ class RegionSensor(CoordinatorEntity, SensorEntity):
         self._metric: str = metric
         self._attr_unique_id = f"{period}_{metric}_{region}".lower()
         self._attr_name = f"{period} {metric} {region}".upper()
-        self._attr_native_unit_of_measurement = "$/MWh"
+        self._attr_state_class = SensorStateClass.MEASUREMENT
+        self._attr_native_unit_of_measurement = METRICS[self._metric]["unit"]
+        self._attr_suggested_display_precision = METRICS[self._metric]["display_precision"]
+        self._attr_device_class = METRICS[self._metric]["device_class"]
         self._attr_icon = "mdi:chart-line"
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, region)},
+            name=f"NEM {region}",
+            manufacturer="AEMO",
+            model="AEMO NEM",
+            configuration_url="https://www.aemo.com.au/energy-systems/electricity/national-electricity-market-nem/data-nem/data-dashboard-nem",
+        )
 
-    def _rows(self):
-        return (
-            [
-                r[self._metric]
+    def _series(self):
+        if self.coordinator.data["5MIN"]:
+            return [
+                [r["SETTLEMENTDATE"], r[self._metric]]
                 for r in self.coordinator.data["5MIN"]
                 if r["REGION"] == self._region and r["PERIODTYPE"] == self._period
             ]
-            if self.coordinator.data["5MIN"]
-            else None
-        )
 
     @callback
     def _handle_coordinator_update(self) -> None:
-        rows = self._rows()
-        if rows:
-            self._attr_native_value = rows[-1 if self._period == "ACTUAL" else 0]["RRP"]
-            self._attr_extra_state_attributes = {
-                "series": [[r["SETTLEMENTDATE"], r[self._metric]] for r in rows]
-            }
+        series = self._series()
+        if series:
+            self._attr_native_value = series[-1 if self._period == "ACTUAL" else 0][1]
+            self._attr_extra_state_attributes = { "series": series }
         else:
             self._attr_native_value = None
             self._attr_extra_state_attributes = {}
